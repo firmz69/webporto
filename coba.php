403dc42edba5b7156563dd0cffd3cbc51eec85ee
firmz69 <105659061+firmz69@users.noreply.github.com>
2022-07-14 18:21:17 +0700

    Add files via upload

---
+++ coba.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+
+    <!-- my styles.css-->
+    <link rel="stylesheet" href="style.css" />
+
+    <!-- import font -->
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet" />
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <title>Home</title>
+</head>
+<body>
+    <!-- Awal Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Firman</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item active nav-link" href="#">Home <span class="sr-only">(current)</span></a
+            >
+            <a class="nav-item nav-link" href="#about">About</a>
+                    <a class="nav-item tombol btn btn-primary" href="#contact-me">HIRE ME</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Awal Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-left">
+            <h1 class="display-4">Hello, I'm a<br />Web <span>Developer</span></h1>
+            <a href="#about" class="btn btn-primary tombol">About Me</a
+        >
+      </div>
+    </div>
+    <!-- Akhir Jumbotron -->
+
+    <!-- Awal Panel -->
+    <div class="container">
+      <div class="pannel-top" id="about">
+        <h1 class="text-center text-white">Things What I Do</h1>
+        <div class="row justify-content-center">
+          <div class="col-10 info-panel">
+            <div class="row">
+              <div class="col-lg">
+                <h1 class="float-left" style="margin-top: -2px">👨🏻‍💻</h1>
+                <h4>Code</h4>
+                <p>
+                  Saya senang belajar bahasa pemograman php,css,html
+                </p>
+              </div>
+              <div class="col-lg">
+                <h1 class="float-left" style="margin-top: -2px">⛹️‍♂️</h1>
+                <h4>Sports</h4>
+                <p>
+                Saya menghabiskan waktu untuk bermain futsal 
+                </p>
+              </div>
+              <div class="col-lg">
+                <h1 class="float-left" style="margin-top: -2px">📖</h1>
+                <h4>Reading</h4>
+                <p>
+                Saya senang membaca cerpen,novel,koran dll
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Akhir Panel -->
+</br>
+</br>
+      <!-- Awal Bio -->
+      <div class="row workingspace">
+        <div class="col-lg-7">
+          <img
+            src="img/jos.png"
+            alt="Workingspace"
+            class="img-fluid rounded"
+          />
+        </div>
+        <div class="col-lg-4">
+          <h3>Hi Bob!<span></h3>
+          <p>
+            Nama saya fireman umur saya 17 tahun sekolah di smk muhammadiyah 1 Yogyakarta 
+            Saya seorang Pengembang Front-End yang antusias,kreatif,dan mandiri
+          </p>
+          <a href="#contact-me" class="btn btn-primary tombol">Hire Me</a>
+        </div>
+    </div>
+    <!-- Akhir Bio -->
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<!-- Awal Contact -->
+<div class="row profile">
+        <div class="col-lg-20" id="contact-me">
+        </div> 
+        </div> 
+        <div class="text-center">
+            <h3>☎Contact</span> Me</h3>
+            <ul class="list-unstyled">
+            <a class="text-dark">📞+6287832456615</a>
+            </br>
+            <a class="text-dark">📩 firmsnadi@gmail.com</a>
+          </div>
+        </div>
+    </div>
+    <!-- Akhir Contact -->
+    <!-- Awal Footer -->
+    <div class="row footer">
+        <div class="col text-center">
+            <p>©️2022 by Firman</p>
+        </div>
+    </div>
+    <!-- Akhir Footer -->
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
